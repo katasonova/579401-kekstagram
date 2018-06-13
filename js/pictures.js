8eff55bc70e6206801010cf40bafbb7c2aadc8eb
Ekata <yogaelen108@gmail.com>
2018-06-13 21:12:46 +0100

    2.1 - 2.3 tasks are done

--- js/pictures.js
+++ js/pictures.js
@@ -1,53 +1,93 @@
-var photos = [];
-var maxPhotos = 25;
+'use strict';
 
-var similarPhotosItem = document.querySelector('#picture')
-  .content
-  .querySelector('.picture__img');
+var PHOTOS_COUNT = 25;
 
+var PHOTO_PATH = 'photos/';
+var PHOTO_EXTENSION = '.jpg';
 
-var createPhotosItem = function (PhotosNumber) {
+var LIKES = {
+  MIN: 15,
+  MAX: 200
+};
 
-  var photoElement = similarPhotosItem.cloneNode(true);
+var COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
 
-    //url, строка — адрес картинки вида photos/{{i}}.jpg, где {{i}} это ////число от 1 до 25. Адреса картинок не должны повторяться.
-    for (var i = 1, i <= maxPhotos; i++) {
-      photosItem.url = i;
-    }
+var COMMENT_MIN = 0;
+var COMMENT_MAX = COMMENTS.length - 1;
+var COMMENT_ONE = 1;
+var COMMENT_TWO = 2;
 
-    //likes, число — количество лайков, поставленных фотографии. Случайное число от 15 до 200
-    var maxLikes = 200;
-    var minLikes = 15;
+var DESCRIPTION = [
+  'Тестим новую камеру!',
+  'Затусили с друзьями на море',
+  'Как же круто тут кормят',
+  'Отдыхаем...',
+  'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
+  'Вот это тачка!'
+];
 
-    photosItem.likes = Math.floor(Math.random() * (maxLikes - minLikes + 1)) + minLikes;
+var DESCRIPTION_MIN = 0;
+var DESCRIPTION_MAX = DESCRIPTION.length - 1;
 
-    //comments, массив строк — список комментариев, оставленных другими пользователями к этой фотографии.
+var photoTemplate = document.querySelector('#picture').content;
+var picturesElement = document.querySelector('.pictures');
 
-    for (var i = 0; i < comments.length; i++) {
-      for (var j = 1; j < comments.length - 1; j++) {
-        comments[i] = 'comment' + j;
-      }
-    }
+var getPhotoPath = function (numberOfPhoto) {
+  return PHOTO_PATH + numberOfPhoto + PHOTO_EXTENSION;
+};
 
-    photosItem.comments = comments[Math.floor(Math.random() * comments.length;
+var getRandomIntFromRange = function (min, max) {
+  return  Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-          //description, строка, в которой есть одно из следующих предложений:
-          var description = [];
-          var description1 = 'Тестим новую камеру!';
-          var description2 = 'Затусили с друзьями на море';
-          var description3 = 'Как же круто тут кормят ';
-          var description4 = 'Отдыхаем...';
-          var description5 = 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......';
-          var description6 = 'Вот это тачка!';
+var getCommentsForPhoto = function (count) {
+  var result = [];
 
-          for (var i = 0; i < description.length; i++) {
-            for (var j = 1; j < description.length - 1; j++) {
-              comments[i] = 'description' + j;
-            }
-          }
+  for (var i = 0; i < count; i++) {
+    result.push(COMMENTS[getRandomIntFromRange(COMMENT_MIN, COMMENT_MAX)]);
+  } 
 
-          photosItem.description = description[Math.floor(Math.random() * description.length;};
+  return result;
+};
 
-            //На основе данных, созданных в предыдущем пункте и шаблона #picture создайте DOM-элементы, соответствующие фотографиям и заполните их данными из массива: var similarPhotosItem = document.querySelector('#picture')
-            //.content
-            //.querySelector('.picture__img');
+var createPhotoObject = function () {
+  var result = [];
+
+  for (var i = 1; i <=  PHOTOS_COUNT; i++) {
+    var newPhoto = {};
+    newPhoto.url = getPhotoPath(i);
+    newPhoto.likes = getRandomIntFromRange(LIKES.MIN, LIKES.MAX);
+    newPhoto.comments = getCommentsForPhoto(getRandomIntFromRange(COMMENT_ONE, COMMENT_TWO));
+    newPhoto.description = DESCRIPTION[getRandomIntFromRange(DESCRIPTION_MIN, DESCRIPTION_MAX)];
+
+    result.push(newPhoto);
+  }
+
+  return result;
+};
+
+var renderPhotos = function (photos) {
+  var documentFragment = document.createDocumentFragment();
+
+  for (var i = 0; i < photos.length - 1; i++) {
+    var newPhotoNode = photoTemplate.cloneNode(true);
+    newPhotoNode.querySelector('.picture__img').src = photos[i].url;
+    newPhotoNode.querySelector('.picture__stat--likes').textContent = photos[i].likes;
+    newPhotoNode.querySelector('.picture__stat--comments').textContent = photos[i].comments.length;
+
+    documentFragment.appendChild(newPhotoNode);
+  }
+
+  picturesElement.appendChild(documentFragment);
+};
+
+var allPhotos = createPhotoObject();
+console.log(allPhotos);
+renderPhotos(allPhotos);
