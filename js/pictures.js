50ae810370fc5b09eb690f9475e77dde47a59778
Ekata <yogaelen108@gmail.com>
2018-06-26 13:29:04 +0100

    Added uploading an image and displaying an editing form, problems with 'change' on esc

--- js/pictures.js
+++ js/pictures.js
@@ -47,7 +47,7 @@ var AVATAR_EXTENSION = '.svg';
 var ESC_KEYCODE = 'Escape';
 
 var existBigPictureElement = document.querySelector('.big-picture');
-existBigPictureElement.classList.remove('hidden');
+// existBigPictureElement.classList.remove('hidden');
 
 var mainContainer = document.querySelector('main');
 
@@ -80,6 +80,7 @@ var getCommentsForPhoto = function (count) {
   return result;
 };
 
+// создание массива объектов, объект - одна фотография
 var createPhotoObject = function () {
   var result = [];
 
@@ -96,6 +97,7 @@ var createPhotoObject = function () {
   return result;
 };
 
+// отрисовка всех фотографий на страницу
 var renderPhotos = function (photos) {
   var documentFragment = document.createDocumentFragment();
 
@@ -111,6 +113,7 @@ var renderPhotos = function (photos) {
   picturesElement.appendChild(documentFragment);
 };
 
+
 var allPhotos = createPhotoObject();
 renderPhotos(allPhotos);
 
@@ -118,34 +121,44 @@ renderPhotos(allPhotos);
 var getAvatarPath = function (numberOfAvatar) {
   return AVATAR_PATH + numberOfAvatar + AVATAR_EXTENSION;
 };
+// .social__comments два раза нахожу элемент с этим классов один раз в функ renderTargetPhoto другой раз здесьпотому что нужна ссылка на объект
+// var existSocialCommentElement = existBigPictureElement.querySelector('.social__comments');
+var socialCommentElement = existBigPictureElement.querySelector('.social__comments');
 
-var createSocialComments = function (comments) {
-  var result = [];
+var createSocialComments = function (commentList, comments) {
+
+  var documentFragment = document.createDocumentFragment();
+  // var result = [];
 
   for (var i = 0; i < comments.length; i++) {
-    var socialCommentItem = document.createElement('li');
-    socialCommentItem.classList.add('social__comment');
+  var socialCommentItem = document.createElement('li');
+  socialCommentItem.className = 'social__comment';
 
-    var socialCommentAvatar = document.createElement('img');
-    socialCommentAvatar.src = '';
-    socialCommentAvatar.classList.add('social__picture');
-
-    socialCommentItem.appendChild(socialCommentAvatar);
-
-    var socialCommentText = document.createElement('p');
-    socialCommentText.textContent = comments[i];
-    socialCommentText.classList.add('social__text');
-
-    socialCommentItem.appendChild(socialCommentText);
-
-    result.push(socialCommentItem);
+  var socialCommentAvatar = document.createElement('img');
+  socialCommentAvatar.src = '';
+  socialCommentAvatar.className = 'social__picture social__comment--text';
+  socialCommentAvatar.alt = 'Аватар комментатора фотографии';
+  socialCommentAvatar.width = '35';
+  socialCommentAvatar.height = '35';
+  
+  socialCommentItem.appendChild(socialCommentAvatar);
+  
+  var socialCommentText = document.createElement('p');
+  socialCommentText.textContent = comments[i];
+  socialCommentText.className = 'social__text';
+  
+  socialCommentItem.appendChild(socialCommentText);
+  
+  documentFragment.appenChild(socialCommentItem);
+  
+  // result.push(socialCommentItem);
   }
 
-  return result;
+  socialCommentElement.appendChild(documentFragment);
+
+  // return result;
 };
 
-createSocialComments(COMMENTS);
-console.log('privet');
 
 var renderTargetPhoto = function (photoObject) {
   var bigPictureElementClone = existBigPictureElement.cloneNode(true);
@@ -156,24 +169,45 @@ var renderTargetPhoto = function (photoObject) {
   bigPictureElementClone.querySelector('.likes-count').textContent = photoObject.likes;
   bigPictureElementClone.querySelector('.comments-count').textContent = photoObject.comments.length;
   bigPictureElementClone.querySelector('.social__picture').src = getAvatarPath(getRandomIntFromRange(AVATAR.MIN, AVATAR.MAX));
-  bigPictureElementClone.querySelector('.social__text').textContent = photoObject.comments;
-  // bigPictureElementClone.querySelector('.social__comment').textContent = getCommentsForPhoto(photoObject);
   bigPictureElementClone.querySelector('.social__caption').textContent = photoObject.description;
 
   var socialCommentElement = bigPictureElementClone.querySelector('.social__comments');
-  socialCommentElement.
+  socialCommentElement.innerHTML = '';
+  createSocialComments(socialCommentElement, photoObject.comments);
+
 
   mainContainer.appendChild(bigPictureElementClone);
 
   return bigPictureElementClone;
 };
 
-renderTargetPhoto(allPhotos[0]);
 
+var openChangePhotoForm = function () {
+  imgUploadOverlay.classList.remove('hidden');
+  document.addEventListener('keydown', escKeyboardButtonHandler);
+};
+// renderTargetPhoto(allPhotos[0]);
 
 var photoUpload = document.querySelector('#upload-file');
+photoUpload.addEventListener('change', openChangePhotoForm);
+
 var imgUploadOverlay = document.querySelector('.img-upload__overlay');
-var photoUploadClose = imgUploadOverlay.querySelector('.upload-cancel');
+var photoUploadClose = imgUploadOverlay.querySelector('#upload-cancel');
+
+/*// сбрас значение поля выбора файла #upload-file
+var inputId = 'img-upload__start';
+
+var clearFileInputField = function (element) { 
+  document.querySelector(element).innerHTML = document.querySelector(element).innerHTML; 
+};*/
+
+var closeChangePhotoForm = function () {
+  imgUploadOverlay.classList.add('hidden');
+  document.removeEventListener('keydown', escKeyboardButtonHandler);
+  // clearFileInputField(inputId);
+};
+
+photoUploadClose.addEventListener('click', closeChangePhotoForm);
 
 var escKeyboardButtonHandler = function (evt) {
   if (evt.key === ESC_KEYCODE) {
@@ -181,17 +215,109 @@ var escKeyboardButtonHandler = function (evt) {
   }
 };
 
+
+/*var clickPhotoHandler = function (photoObject, evt) {
+  evt.preventDefault();
+  renderTargetPhoto(photoObject);
+}
+
+var renderPhotos = function (photos) {
+  var documentFragment = document.createDocumentFragment();
+
+  for (var i = 0; i < photos.length - 1; i++) {
+    var newPhotoNode = photoTemplate.cloneNode(true);
+    newPhotoNode.querySelector('.picture__img').src = photos[i].url;
+    newPhotoNode.querySelector('.picture__stat--likes').textContent = photos[i].likes;
+    newPhotoNode.querySelector('.picture__stat--comments').textContent = photos[i].comments.length;
+
+    newPhotoNode.querySelector('.picture__link').addEventListener('click', clickPhotoHandler.bind(undefined, photos[i]));
+
+    documentFragment.appendChild(newPhotoNode);
+  }
+
+  picturesElement.appendChild(documentFragment);
+};
+
+var getAvatarPath = function (numberOfAvatar) {
+  return AVATAR_PATH + numberOfAvatar + AVATAR_EXTENSION;
+};
+
+var allPhotos = createPhotoObject();
+renderPhotos(allPhotos);
+
+var createSocialComents = function (comments) {
+  var result = [];
+
+  for (var i = 0; i < comments.length; i++) {
+    var socialComment = document.createElement('li');
+    socialComment.className = 'social__comment';
+
+    var socialImg = document.createElement('img');
+    socialImg.src = '';
+    socialImg.className = 'social__picture';
+
+    socialComment.appendChild(socialImg);
+
+    var socialText = document.createElement('p');
+    socialText.textContent = comments[i];
+    socialText.className = 'social__text';
+
+    socialComment.appendChild(socialText);
+
+    result.push(socialComment);
+  }
+
+  return result;
+}
+
+var renderTargetPhoto = function (photoObject) {
+  var existBigPictureElement = document.querySelector('.big-picture');
+  var bigPictureElement = existBigPictureElement.cloneNode(true);
+
+  mainContainer.removeChild(existBigPictureElement);
+
+  bigPictureElement.querySelector('.big-picture__img').querySelector('img').src = photoObject.url;
+  bigPictureElement.querySelector('.likes-count').textContent = photoObject.likes;
+  bigPictureElement.querySelector('.comments-count').textContent = photoObject.comments.length;
+  bigPictureElement.querySelector('.social__picture').src = getAvatarPath(getRandomIntFromRange(AVATAR.MIN, AVATAR.MAX));
+  // photoView.querySelector('social__text').textContent = photoObject.comments;
+  bigPictureElement.querySelector('.social__caption').textContent = photoObject.description;
+  var socialCommentElement = bigPictureElement.querySelector('.social__comments');
+
+  // Здесь поставь обработчик на закрытие по крестику, esc
+  bigPictureElement.querySelector('.big-picture__cancel').addEventListener('click', function (evt) {
+    bigPictureElement.classList.add('hidden');
+  });
+
+  bigPictureElement.classList.remove('hidden');
+
+  mainContainer.appendChild(bigPictureElement);
+
+  return photoView;
+};
+
 var openChangePhotoForm = function () {
   imgUploadOverlay.classList.remove('hidden');
   document.addEventListener('keydown', escKeyboardButtonHandler);
 };
+// renderTargetPhoto(allPhotos[0]);
+
+var photoUpload = document.querySelector('#upload-file');
+
+photoUpload.addEventListener('change', openChangePhotoForm);
+
+var imgUploadOverlay = document.querySelector('.img-upload__overlay');
+var photoUploadClose = imgUploadOverlay.querySelector('#upload-cancel');
 
 var closeChangePhotoForm = function () {
   imgUploadOverlay.classList.add('hidden');
   document.removeEventListener('keydown', escKeyboardButtonHandler);
 };
 
-photoUploadClose.addEventListener('click', closeChangePhotoForm());
+photoUploadClose.addEventListener('click', closeChangePhotoForm);
 
-photoUpload.addEventListener('change', openChangePhotoForm());
-
+var escKeyboardButtonHandler = function (evt) {
+  if (evt.key === ESC_KEYCODE) {
+    closeChangePhotoForm();
+  }
+};*/
